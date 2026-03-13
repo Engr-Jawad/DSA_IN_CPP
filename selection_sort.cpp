1c59299c7a5504cbc06304bd9379cdfebda402fc
Engr-Jawad <jawadbuniri2@gmail.com>
2026-03-13 18:11:18 +0500

    algorithm change

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,28 +1,37 @@
 #include<iostream>
 using namespace std;
-void selection_sort(int arr[],int size){
-    for (int i=0;i<size;i++){
-        for(int j=i+1;j<size;j++){
-            if (arr[j]<arr[i]){
-                swap(arr[i],arr[j]);
+
+void selection_sort(int array[],int n){
+    for (int i=0; i<n-1;i++){
+        int smallestindx=i;
+        for (int j=i+1;j<n;j++){
+            if (array[j] < array[smallestindx]){
+                smallestindx=j;
             }
         }
+        swap(array[i],array[smallestindx]);
     }
 }
+
+void print(int array[],int n){
+    for (int i=0;i<n;i++){
+        cout<<array[i];
+    }
+}
+
 int main(){
-    int size;
-    cout<<"enter size of the array : ";
-    cin>>size;
-    int arr[size];
-   
-    for (int i=0;i<size;i++){
-         cout<<"enter elements of the array : ";
-        cin>>arr[i];
+    int n;
+    cout<<"enter  the size of array : ";
+    cin>>n;
+    int array[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter elements in the array : ";
+        cin>>array[i];
     }
-    selection_sort(arr,size);
-    cout<<"sorted array is : ";
-    for (int i=0;i<size;i++){
-        cout<<arr[i]<< " ";
-    }
+
+    selection_sort(array,n);
+    print(array,n);
     return 0;
+
+
 }

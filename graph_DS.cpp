24b827f23845928162082a8a469f6019e17cd07b
Engr-Jawad <jawadbuniri2@gmail.com>
2026-04-05 18:20:03 +0500

    grpah data structure intro

--- graph_DS.cpp
+++ graph_DS.cpp
@@ -62,11 +62,15 @@ class graph {
     }
 
     void addedge(int v,int u){
+         if (u >= this->v || v >= this->v) {
+            cout << "Error: Vertex out of range!" << endl;
+            return;
+        }
         l[u].push_back(v);
         l[v].push_back(u);
     }
     void adjlist(){
-        for (int i= 0; i<v;i++){
+        for (int i= 1; i<v;i++){
             cout<< i << " : ";
             for (int neigh : l[i]){
                 cout<< neigh << " : ";
@@ -77,11 +81,16 @@ class graph {
     }
 };
 int main(){
-    graph g(4);
-    g.addedge(0,1);
+    graph g(8);
     g.addedge(1,2);
-    g.addedge(1,3);
-    g.addedge(2,0);
+    g.addedge(2,3);
+    g.addedge(3,4);
+    g.addedge(4,5);
+    g.addedge(4,6);
+    g.addedge(5,4);
+    g.addedge(6,4);
+    g.addedge(6,7);
+
     g.adjlist();
     return 0;
 }

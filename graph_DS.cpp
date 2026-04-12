128854008da92ac298f780792377e5b0b0bf7326
Engr-Jawad <jawadbuniri2@gmail.com>
2026-04-12 21:39:28 +0500

    bfs and dfs

--- graph_DS.cpp
+++ graph_DS.cpp
@@ -48,6 +48,7 @@
 #include<iostream>
 #include<vector>
 #include<list>
+#include<queue>
 using namespace std;
 class graph{
     int v;
@@ -62,28 +63,66 @@ class graph{
         l[vertex_two].push_back(vertex_one);
 
     }
-    void adjacencylist(){
+     void adjacencylist(){
         for (int i=1;i<v;i++){
             cout<<i<<" : ";
-            for(int neigh : l[i]){
+             for(int neigh : l[i]){
                 cout<<neigh <<" : ";
 
-            }
-            cout<<endl;
+             }
+             cout<<endl;
         }
+     }
+
+    // breadth first search
+    void bfs(){
+        queue<int> Q;
+        vector<bool> vis(v,false); // boolean array 
+        Q.push(0); // source node eaquall to 0
+        vis[0]=true;
+        while (Q.size() > 0){
+            int source = Q.front();
+            Q.pop();
+            cout<<source<< " ";
+            for (int v : l[source]){
+                if (!vis[v]){
+                    vis[v]=true;
+                    Q.push(v);
+                }
+            }
+        }
+        cout<<endl;
+    }
+    void dfshelper(int source,vector<bool> &vis){
+        cout<<source<< " ";
+        vis[source]=true;
+        for(int vertice : l[source]){
+            if (!vis[vertice]){
+                dfshelper(vertice,vis);
+            }
+        }
+    }
+    void dfs(){
+        int src=0;
+        vector<bool> vis(v,false);
+        dfshelper(src,vis);
     }
 };
 int main(){
-    graph g(8);
+    graph g(7);
+    g.addedge(0,1);
     g.addedge(1,2);
-    g.addedge(2,3);
-    g.addedge(3,4);
-    g.addedge(4,5);
-    g.addedge(4,6);
-    g.addedge(5,4);
-    g.addedge(6,4);
-    g.addedge(6,7);
+    g.addedge(1,3);
+    g.addedge(2,1);
+    g.addedge(2,4);
+    g.addedge(3,6);
+
+   
 
     g.adjacencylist();
+    cout<<"breadth first search\n";
+    g.bfs();
+    cout<<"deafth first search \n";
+    g.dfs();
     return 0;
 }
